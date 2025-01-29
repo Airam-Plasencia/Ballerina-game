dfe188ee4d3e81da2e81a19cf8204c4cce6af146
Airam Plasencia <airaplas@gmail.com>
2025-01-29 11:15:10 +0000

    All Animations Add

--- proy-Ballerina/js/players.js
+++ proy-Ballerina/js/players.js
@@ -9,8 +9,11 @@ class Player {
     this.directionY = 0;
     this.isJumping = false;
     this.isDodging = false;
-    this.jumpFrames = ["./images/Ballerina_02.png",];
+    this.jumpFrames = ["./images/Ballerina_04.png"];
+    
     this.currentJumpFrame = 0;
+    this.currentLeftFrame = 0;
+    this.currentRightFrame = 0;
     this.element = document.createElement("img");
 
     this.element.src = imgSrc;
@@ -24,82 +27,111 @@ class Player {
 
     this.jumpInterval = null;
     this.dodgetimeout = null;
+
+    this.leftImages = [
+      
+      "./images/Ballerina_05.png"
+    ];
+    this.rightImages = [
+      
+      "./images/Ballerina_02.png"
+    ];
+    
+    
+    this.animationInterval = null;
   }
+
   startJumping() {
     if (this.isJumping) return;
     this.isJumping = true;
     this.directionY = -10;
     this.currentJumpFrame = 0;
+    this.updateJumpImage();
   }
 
   stopJumping() {
     this.isJumping = false;
     this.directionY = 0;
-    this.element.src = "./images/Ballerina_01.png";
+    this.element.src = "./images/Ballerina_01.png"; 
   }
 
   startDodging() {
     if (this.isDodging) return;
     this.isDodging = true;
     this.directionY = 4;
-    this.element.src = "./images/Ballerina_03.png"
+    this.element.src = "./images/Ballerina_03.png"; 
   }
 
   stopDodging() {
     this.isDodging = false;
     this.directionY = 0;
-    this.element.src = "./images/Ballerina_01.png";
+    this.element.src = "./images/Ballerina_01.png"; 
     clearTimeout(this.dodgeTimeout);
   }
-  move() {
 
+  move() {
     this.left += this.directionX;
     this.top += this.directionY;
 
     if (this.isJumping) {
-      this.element.src = this.jumpFrames[this.currentJumpFrame];
-      this.currentJumpFrame++;
-
-      if (this.currentJumpFrame >= this.jumpFrames.length) {
-        this.currentJumpFrame = 0;
+      this.updateJumpImage();
+    }
+   
+    if (this.directionX < 0) {
+      this.currentLeftFrame++;
+      if (this.currentLeftFrame >= this.leftImages.length) {
+        this.currentLeftFrame = 0; 
       }
-    } else if (!this.isDodging) {
+      this.element.src = this.leftImages[this.currentLeftFrame];
+    } 
+    
+    else if (this.directionX > 0) {
+      this.currentRightFrame++;
+      if (this.currentRightFrame >= this.rightImages.length) {
+        this.currentRightFrame = 0; 
+      }
+      this.element.src = this.rightImages[this.currentRightFrame];
+    } 
+    
+    else if (!this.isJumping && !this.isDodging) {
       this.element.src = "./images/Ballerina_01.png";
-    } else {
-      this.element.src = "./images/Ballerina_03.png";
     }
 
+    
     if (this.left < 10) {
       this.left = 10;
     }
-
 
     if (this.top < 10) {
       this.top = 10;
     }
 
-
     if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
       this.left = this.gameScreen.offsetWidth - this.width - 10;
     }
-
 
     if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
       this.top = this.gameScreen.offsetHeight - this.height - 10;
     }
 
-
     this.updatePosition();
+  }
+  updateJumpImage() {
+    // Si estamos saltando, usamos la imagen del salto
+    if (this.isJumping) {
+      this.element.src = this.jumpFrames[this.currentJumpFrame];
+      // Si tienes más imágenes para el salto, puedes agregar más aquí
+    }
   }
 
   updatePosition() {
     this.element.style.left = `${this.left}px`;
     this.element.style.top = `${this.top}px`;
   }
+
   didCollide(obstacle) {
     const playerRect = this.element.getBoundingClientRect();
     const obstacleRect = obstacle.element.getBoundingClientRect();
-
 
     return !(playerRect.right < obstacleRect.left ||
       playerRect.left > obstacleRect.right ||
--- proy-Ballerina/js/script.js
+++ proy-Ballerina/js/script.js
@@ -2,15 +2,12 @@ window.onload = function () {
   const startButton = document.getElementsByClassName("start-button")[0];
   const restartButton = document.getElementsByClassName("restart-button")[0];
   restartButton.addEventListener("click", function () {
-
     restartGame();
   });
-
 
   function restartGame() {
     location.reload();
   }
-
 
   let game;
 
@@ -22,8 +19,8 @@ window.onload = function () {
     game = new Game();
     game.start();
   }
-  function handleKeydown(event) {
 
+  function handleKeydown(event) {
     const key = event.key;
     const possibleKeystrokes = [
       "ArrowUp",
@@ -32,10 +29,8 @@ window.onload = function () {
       "ArrowRight",
     ];
 
-
     if (possibleKeystrokes.includes(key)) {
       event.preventDefault();
-
 
       switch (key) {
         case "ArrowUp":
@@ -46,15 +41,14 @@ window.onload = function () {
           break;
         case "ArrowLeft":
           game.player.directionX = -5;
-          game.player.element.src = "./images/Ballerina_04.png";
           break;
         case "ArrowRight":
           game.player.directionX = 5;
-          game.player.element.src = "./images/Ballerina_05.png";
           break;
       }
     }
   }
+
   function handleKeyup(event) {
     const key = event.key;
 
@@ -64,10 +58,9 @@ window.onload = function () {
       game.player.stopDodging();
     } else if (key === "ArrowLeft" || key === "ArrowRight") {
       game.player.directionX = 0;
-      game.player.element.src = "./images/Ballerina_01.png";
     }
   }
+
   window.addEventListener("keydown", handleKeydown);
   window.addEventListener("keyup", handleKeyup);
-  
 };
